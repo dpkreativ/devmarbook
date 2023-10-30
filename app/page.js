7f76c43ca89115028dbb6706d1120ab6d6584b78
Divine Orji <dpkreativ@gmail.com>
2023-10-30 18:53:57 +0100

    adds book and author details

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import book from '@/assets/book.png';
+import author from '@/assets/trust_profile.png';
 import Button from '@/components/button';
 
 export default function Home() {
@@ -24,7 +25,7 @@ export default function Home() {
           <h3>A comprehensive approach to reaching and engaging developers</h3>
 
           {/* Order links */}
-          <div className="grid md:flex gap-10">
+          <div className="grid md:flex gap-5 md:gap-10">
             <Button>Order on Amazon</Button>
             <Button>Download a free chapter</Button>
           </div>
@@ -32,11 +33,35 @@ export default function Home() {
       </section>
 
       {/* About the book */}
-      <section>
-        <div className="grid gap-10">
-          <h2 className="font-bold text-xl">About the book</h2>
-          <p>{/* Text description */}</p>
-          <div>{/* Embedded video description */}</div>
+      <section className="grid gap-10">
+        <h2 className="font-bold text-xl">What's inside</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+          temporibus fugit ratione. Labore optio error quaerat facilis sit
+          voluptates minima deleniti voluptatum molestias natus soluta vero
+          earum laboriosam, dicta in?
+        </p>
+        <div className="aspect-video bg-slate-200 rounded-lg">
+          {/* Embedded video description */}
+        </div>
+      </section>
+
+      {/* About the author */}
+      <section className="grid gap-10 md:grid-cols-2 items-center">
+        {/* Intro and author bio text */}
+        <div className="grid gap-5 h-max">
+          <h2 className="text-5xl font-bold">About Trust</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            praesentium magnam alias odit eligendi consequuntur non iusto
+            consequatur. Deserunt, ad obcaecati pariatur exercitationem nam
+            quasi dicta accusamus magnam laborum vitae!
+          </p>
+        </div>
+
+        {/* Author image */}
+        <div className="aspect-square bg-slate-400 rounded-full overflow-clip">
+          <Image src={author} alt="Trust Onyekwere" />
         </div>
       </section>
     </main>
