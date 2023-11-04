4fef913491d57ba5372c951f3bf67a737caec6c5
Divine Orji <dpkreativ@gmail.com>
2023-11-04 09:43:00 +0100

    restart

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Button from '@/components/button';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,22 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {/* Header */}
-        <header className="w-full max-w-4xl mx-auto p-5 flex justify-between">
-          <div className="font-bold">{/* Brand */}</div>
-
-          <div className="flex items-center gap-5">
-            <nav>
-              <div>What's inside</div>
-            </nav>
-            <Button>Order on Amazon</Button>
-          </div>
-        </header>
-
-        {/* Main content */}
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
