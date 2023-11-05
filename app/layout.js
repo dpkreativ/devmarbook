b57d9fdc455ef4ba27a0dc95823f67b6730cf9c7
Divine Orji <dpkreativ@gmail.com>
2023-11-05 21:27:59 +0100

    sets up layout for the website

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Button from '@/components/button';
+import { PlanetIcon } from '@/assets';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +13,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#EFEFEF]`}>
+        {/* Header */}
+        <header className="p-5 w-full max-w-7xl mx-auto flex justify-between items-center sticky top-0 z-10">
+          <div>
+            <PlanetIcon />
+          </div>
+          <Button>Menu</Button>
+        </header>
+
+        {/* Main content */}
+        {children}
+      </body>
     </html>
   );
 }
