7ab7528f7ccc0aa96ae8ea183d8da52df2121e76
Divine Orji <dpkreativ@gmail.com>
2023-11-11 15:45:34 +0100

    ui updates

--- components/carousel.jsx
+++ components/carousel.jsx
@@ -5,7 +5,7 @@ export default function Carousel({ children }) {
   const [ref] = useEmblaCarousel();
   return (
     <div className="overflow-hidden w-full max-w-3xl mx-auto" ref={ref}>
-      <div className="flex gap-10">{children}</div>
+      <div className="flex gap-5 md:gap-10">{children}</div>
     </div>
   );
 }
