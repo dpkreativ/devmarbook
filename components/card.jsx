d887eaefb3b51f9bca0f7cc6d5cefc993f4c5524
Divine Orji <dpkreativ@gmail.com>
2023-11-14 03:36:05 +0100

    updates

--- components/card.jsx
+++ components/card.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export function ReviewCard({ content, fullName, img, role }) {
   return (
-    <div className="p-5 w-56 md:w-64 h-full grid gap-3 bg-slate-200 rounded-2xl shadow-2xl">
+    <div className="p-5 w-56 md:w-64 h-full grid gap-3 bg-slate-200 rounded-2xl shadow-xl">
       <div className="w-full flex gap-3">
         {/* user image */}
         <div className="aspect-square h-12 rounded-full bg-slate-400 relative overflow-clip">
--- components/carousel.jsx
+++ components/carousel.jsx
@@ -4,7 +4,10 @@ import useEmblaCarousel from 'embla-carousel-react';
 export default function Carousel({ children }) {
   const [ref] = useEmblaCarousel();
   return (
-    <div className="overflow-hidden w-full max-w-3xl mx-auto" ref={ref}>
+    <div
+      className="overflow-scroll w-full max-w-3xl mx-auto py-5 rounded-2xl"
+      ref={ref}
+    >
       <div className="flex gap-5 md:gap-10">{children}</div>
     </div>
   );
